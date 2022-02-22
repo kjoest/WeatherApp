eafc3250c1391a01ee6a464533b65d41bdb171f9
kyle Joest <kjoest92@gmail.com>
2022-02-22 10:02:25 -0600

    Added visibility and country

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let weather = {
     "apiKey": "97933fae3a9f44d07f02a1a66909b6bc",
-    fetchWeather: function(city) {
+    fetchWeather: function(city, state_code) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" 
-            + city 
+            + city.toUpperCase()
             + "&units=imperial&appid=" 
             + this.apiKey
         )
@@ -13,15 +13,19 @@ let weather = {
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { temp, humidity} = data.main;
+        const { visibility } = data;
         const { speed } = data.wind;
+        const { country } = data.sys;
         document.querySelector(".city").innerText = name;
         document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".visibility").innerText = "Visibility: " + visibility + " ft.";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mi/h";
+        document.querySelector(".country").innerText = "Country: " + country;
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
